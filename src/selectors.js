4ca09ba670c3cf03720f9053bb9cdb90f6c470a2
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-04-20 14:11:01 +0200

    added selectors for modal and newMatch

--- src/selectors.js
+++ src/selectors.js
@@ -4,3 +4,5 @@ export const matchesSelector = state => state.matches;
 export const playersSelector = state => state.players;
 export const tournamentsSelector = state => state.tournaments;
 export const currentItemSelector = state => state.currentItem;
+export const modalOpenSelector = state => state.modalOpen;
+export const newMatchSelector = state => state.newMatch;
