2c0adb4faeed0bfbf93f8feb3348e85b322aac70
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-04-20 14:04:51 +0200

    added modal action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,34 +1,51 @@
-import * as constants from '../constants';
+import {
+  LOAD_MATCHES_DATA,
+  LOAD_PLAYERS_DATA,
+  LOAD_TOURNAMENTS_DATA,
+  DISPLAY_MATCHES,
+  DISPLAY_PLAYERS,
+  DISPLAY_TOURNAMENTS,
+  OPEN_MODAL,
+  CLOSE_MODAL
+} from '../constants';
 
 export function loadMatchesData(payload) {
   return {
-    type: constants.LOAD_MATCHES_DATA,
+    type: LOAD_MATCHES_DATA,
     payload
   };
 }
 
 export function loadPlayersData(payload) {
   return {
-    type: constants.LOAD_PLAYERS_DATA,
+    type: LOAD_PLAYERS_DATA,
     payload
   };
 }
 
 export function loadTournamentsData(payload) {
   return {
-    type: constants.LOAD_TOURNAMENTS_DATA,
+    type: LOAD_TOURNAMENTS_DATA,
     payload
   };
 }
 
 export function displayMatches() {
-  return { type: constants.DISPLAY_MATCHES };
+  return { type: DISPLAY_MATCHES };
 }
 
 export function displayPlayers() {
-  return { type: constants.DISPLAY_PLAYERS };
+  return { type: DISPLAY_PLAYERS };
 }
 
 export function displayTournaments() {
-  return { type: constants.DISPLAY_TOURNAMENTS };
+  return { type: DISPLAY_TOURNAMENTS };
+}
+
+export function openModal() {
+  return { type: OPEN_MODAL };
+}
+
+export function closeModal() {
+  return { type: CLOSE_MODAL };
 }
