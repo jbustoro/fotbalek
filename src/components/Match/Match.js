0490e60f1ff618e12fe8efacae3e68f4336ef746
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-05-09 13:51:47 +0200

    deleted unnecessary

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import dateFormat from 'dateformat'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faArrowUp from '@fortawesome/fontawesome-free-solid/faArrowUp'
+import faArrowDown from '@fortawesome/fontawesome-free-solid/faArrowDown'
 import PropTypes from 'prop-types'
-import { setMatchGain } from './matchHelper'
 import './Match.css'
+
+const setMatchGain = player => (
+  <div>
+    <FontAwesomeIcon
+      icon={player.gain > 0 ? faArrowUp : faArrowDown}
+      style={player.gain > 0 ? { color: '#2ECC40' } : { color: '#FF4136' }}
+    />
+    {` ${Math.floor(player.gain * 100)}`}
+  </div>
+)
 
 class Match extends Component {
   render() {
