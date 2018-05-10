8fc37c0449e8165ce66b81eaab29303e54b9f637
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-05-10 15:10:43 +0200

    added selected atribute for current tournament

--- src/components/SelectTournament/SelectTournament.js
+++ src/components/SelectTournament/SelectTournament.js
@@ -4,27 +4,32 @@ import _ from 'lodash'
 import dateFormat from 'dateformat'
 import { FormControl } from 'react-bootstrap'
 import PropTypes from 'prop-types'
-import { tournamentsSelector } from '../../selectors'
+import { tournamentsSelector, currentTournamentSelector } from '../../selectors'
 
 const mapStateToProps = state => ({
-  tournaments: tournamentsSelector(state.load)
+  tournaments: tournamentsSelector(state.load),
+  currentTournament: currentTournamentSelector(state.display)
 })
 
 class SelectTournament extends Component {
   render() {
-    const { tournaments, onChange } = this.props
+    const { tournaments, currentTournament, onChange } = this.props
 
     return (
       <FormControl componentClass="select" onChange={onChange}>
         <option>Select a tournament</option>
-        {_.map(tournaments, (tournament, key) => (
-          <option key={key} value={key}>
-            {dateFormat(
-              tournament.createdAt,
-              'dddd, mmmm dS, yyyy, h:MM:ss TT'
-            )}
-          </option>
-        ))}
+        {_.map(tournaments, (tournament, key) => {
+          const selected = currentTournament === key ? true : false
+
+          return (
+            <option key={key} value={key} selected={selected}>
+              {dateFormat(
+                tournament.createdAt,
+                'dddd, mmmm dS, yyyy, h:MM:ss TT'
+              )}
+            </option>
+          )
+        })}
       </FormControl>
     )
   }
@@ -32,6 +37,7 @@ class SelectTournament extends Component {
 
 SelectTournament.propTypes = {
   tournaments: PropTypes.object,
+  currentTournament: PropTypes.string,
   onChange: PropTypes.func
 }
 
